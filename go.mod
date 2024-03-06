c317ce0805a88a063b64bc850a3b3cda09070643
Jongmin Kim <whdalsrnt@megazone.com>
2024-03-07 08:14:24 +0900

    feat: change example code

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.22.1
 require (
 	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
 	rsc.io/quote v1.5.2 // indirect
-	rsc.io/sampler v1.3.0 // indirect
+	rsc.io/quote/v3 v3.1.0 // indirect
+	rsc.io/sampler v1.3.1 // indirect
 )
--- hello.go
+++ hello.go
@@ -1,6 +1,14 @@
 package hello
 
+import (
+	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
+)
+
 func Hello() string {
-	// return quote.Hello()
-	return "Hello, world."
+	return quote.Hello()
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
 }
